a03c18c5057d734c10c8c1d53eae3677a8434eb5
holsapple <raymond.holsapple@gmail.com>
2017-04-14 09:50:56 -0400

    initial commit

--- BUSCOmod.py
+++ BUSCOmod.py
@@ -27,6 +27,8 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
+Modified by R.W. Holsapple (Fall 2016) to try an incorporate the use of BLASTp.
+
 """
 
 import os
